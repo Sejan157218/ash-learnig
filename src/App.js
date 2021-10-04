78666ddb66acf53db92c722dd6a15082b911e354
sejan <sejan157218@gmail.com>
2021-10-04 06:37:46 +0600

    create NabBar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import Service from "./Components/Service/Service";
+import NotFound from "./Components/NotFound/NotFound";
+import MenuBar from './Components/MenuBar/MenuBar';
+import Footer from './Components/Footer/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Router>
+        <MenuBar />
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/about">
+            <About></About>
+          </Route>
+          <Route exact path="/service">
+            <Service></Service>
+          </Route>
+          <Route exact path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/Components/MenuBar/MenuBar.js
@@ -0,0 +1,80 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarkerAlt, faEnvelope, faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import Button from '@restart/ui/esm/Button';
+import React from 'react';
+import { Container, Form, Nav, Navbar } from 'react-bootstrap';
+import "./MenuBar.css";
+import { Link } from 'react-router-dom';
+
+const MenuBar = () => {
+    return (
+        <div className="menubar">
+            {/*  Navigation   */}
+            <Navbar expand="lg" className="nav-container">
+                <Container fluid>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-lg-0 d-flex align-items-center"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Link to="" className="ms-5  text-decoration-none nav-text"><FontAwesomeIcon icon={faMapMarkerAlt} className="me-1" />28 tenth Avenue Dhanmondi, Dhaka</Link>
+                            <Link to="" className="ms-5  text-decoration-none nav-text"><FontAwesomeIcon icon={faEnvelope} className="me-1" />info@gmail.com</Link>
+
+                        </Nav>
+                        <Form className="d-flex w-50 justify-content-end ">
+                            <input
+                                type="search"
+                                placeholder="Search"
+                                className="w-50 p-50 border-0 search-box"
+                            />
+                            <Button className="btn-color pe-5 border-0">LINK/REGISTER</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+
+                {/*  Navigation 2  */}
+            </Navbar>
+            <Navbar className="nav-2" expand="lg">
+                <Container fluid>
+
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Link to="/home" className="nav-2-nav"><li>Home</li></Link>
+
+                        </Nav>
+                        <Nav
+                            className="justify-content-end mt-5 me-5"
+
+                        >
+                            <Link to="/home" className="nav-2-nav"><li>Home</li></Link>
+
+                            <Link to="/about" className="nav-2-nav"><li>About</li></Link>
+                            <Link to="/service" className="nav-2-nav">
+                                <li>Service</li>
+                            </Link>
+                            <Link to="/service" className="nav-2-nav">
+                                <li>Blogs</li>
+                            </Link>
+                            <Link to="/service" className="nav-2-nav">
+                                <li>Contact</li>
+                            </Link>
+                            <Link to="/service" className="nav-2-nav">
+                                <FontAwesomeIcon icon={faShoppingBag} className="me-1" />
+                            </Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+
+    );
+};
+
+export default MenuBar;
